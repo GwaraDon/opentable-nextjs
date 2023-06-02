4170a724a6bffd278efc246da30c2e69c4765f2c
Binay Maharjan <binay.maharjan@ekbana.info>
2023-06-02 10:38:27 +0545

    Implement middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -7,32 +7,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const bearerToken = req.headers["authorization"];
-  if (!bearerToken) {
-    res.status(401).json({ errorMessage: "Unauthorized, No bearer token" });
-    return;
-  }
+  const bearerToken = req.headers["Authorization"] as string;
+
   const token = bearerToken.split(" ")[1];
-  if (!token) {
-    res.status(401).json({ errorMessage: "Unauthorized, No token" });
+
+  const decoded = jwt.decode(token) as { email: string };
+  if (!decoded.email) {
+    res.status(401).json({ errorMessage: "Unauthorized, email" });
     return;
   }
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (e) {
-    res.status(401).json({ errorMessage: "Unauthorized, Invalid token" });
-    return;
-  }
-  const decoded = jwt.decode(token);
-  //   if (!decoded.email || decoded.email === null) {
-  //     res.status(401).json({ errorMessage: "Unauthorized, email" });
-  //     return;
-  //   }
-  if (decoded.email === null) {
-    res.status(401).json({ errorMessage: "Unauthorized, null" });
-    return;
-  }
+
   const user = await prisma.user.findUnique({
     where: {
       email: decoded.email,
